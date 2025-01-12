824edfb69e08f7f2c304f39f4fb3eb761dbfa831
ArmPin01 <145780593+ArmPin01@users.noreply.github.com>
2025-01-12 16:44:48 +0100

    Update ejercicio2.js

--- armando/ejercicio2.js
+++ armando/ejercicio2.js
@@ -1,9 +1,7 @@
 let objeto = {var1:0,var2:10}
-function referencia({var1,var2}=objeto)
-{var1=var1+15;
-    objeto.var1=var1;
- var2=var2-5;
- objeto.var2=var2;
+function referencia(objeto)
+{objeto.var1=12;
+ objeto.var2=12;
 return objeto}
 
 referencia(objeto)
