81b3b33c61a9436b9b46cec26cf689983a72fc70
ArmPin01 <145780593+ArmPin01@users.noreply.github.com>
2025-01-13 19:49:30 +0100

    Create ejercicio5.js

---
+++ armando/ejercicio5.js
@@ -0,0 +1,37 @@
+//5.1 los objetos de la maquina
+let chocobones=
+{
+    Name:"ChocoBon",
+    Fabricante:"Cuetara",
+    Pesogramos:200
+}
+let cocacola=
+{
+    Name:"CocaCola",
+    Fabricante:"CokeCompany",
+    Pesogramos:150
+}
+let bifrutas=
+{
+    Name:"BiFrutas Tropical",
+    Fabricante:"DonSimon",
+    Pesogramos:120
+}
+let donetes=
+{
+    Name:"Donettes",
+    Fabricante:"Bimbo",
+    Pesogramos:350
+}
+//5.2 los carriles de la maquina
+class carril 
+{
+    Precio=0;
+    Producto="Ninguno";
+    Slots=new Array(8)
+}
+//5.3 la maquina
+let maquina=
+{
+    
+}
