f2f731102f5925bdd7352bf88d146e2bf404b316
ArmPin01 <145780593+ArmPin01@users.noreply.github.com>
2025-01-14 19:47:31 +0100

    Update ejercicio5.js

--- armando/ejercicio5.js
+++ armando/ejercicio5.js
@@ -1,61 +1,48 @@
 //5.1 los objetos de la maquina
-let chocobones=
-{
-    Name:"ChocoBon",
-    Fabricante:"Cuetara",
-    Pesogramos:200
-}
-let cocacola=
-{
-    Name:"CocaCola",
-    Fabricante:"CokeCompany",
-    Pesogramos:150
-}
-let bifrutas=
-{
-    Name:"BiFrutas Tropical",
-    Fabricante:"DonSimon",
-    Pesogramos:120
-}
-let donetes=
-{
-    Name:"Donettes",
-    Fabricante:"Bimbo",
-    Pesogramos:350
-}
+let chocobones = {
+  Name: "ChocoBon",
+  Fabricante: "Cuetara",
+  Pesogramos: 200,
+};
+let cocacola = {
+  Name: "CocaCola",
+  Fabricante: "CokeCompany",
+  Pesogramos: 150,
+};
+let bifrutas = {
+  Name: "BiFrutas Tropical",
+  Fabricante: "DonSimon",
+  Pesogramos: 120,
+};
+let donetes = {
+  Name: "Donettes",
+  Fabricante: "Bimbo",
+  Pesogramos: 350,
+};
 //5.2 los carriles de la maquina
-class carril 
-{
-    Precio;
-    Producto;
-    Slots;
+class carril {
+  Precio;
+  Producto;
+  Slots;
 
-    extraer(){
-        for(let i=0;i<obj.railsize;i++)
-        {
-            Slots[i]=Slots[i+1];
-        }
-        return Slots[0]
-    }
+  extraer() {
+    return Slots.pop()
+  }
 
-    rellenar(lista){
-        if (lista.length<=obj.railsize){
-            for(let i=0;i<lista.length;i++)
-                {
-                    Slots[i]=lista[i];
-                }
-        }
-        return Slots
+  rellenar(lista) {
+    if (lista.length <= obj.railsize) {
+      for (let i = 0; i < lista.length; i++) {
+        Slots[i] = lista[i];
+      }
     }
+    return Slots;
+  }
 
-    constructor(obj){
-        Precio=obj.Precio ?? 0;
-        Producto=obj.Producto ?? "Ninguno";
-        Slots=new Array(obj.railsize ?? 8)
-    }
+  constructor(obj) {
+    Precio = obj.Precio ?? 0;
+    Producto = obj.Producto ?? "Ninguno";
+    Slots = new Array(obj.railsize ?? 8);
+  }
 }
 //5.3 la maquina
-let maquina=
-{
-    
-}
+let maquina = {};
