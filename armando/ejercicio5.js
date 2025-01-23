148edd3ba398fa051113647cd6e63fe189270e27
ArmPin01 <145780593+ArmPin01@users.noreply.github.com>
2025-01-23 12:33:57 +0100

    Update ejercicio5.js

--- armando/ejercicio5.js
+++ armando/ejercicio5.js
@@ -26,32 +26,32 @@ class carril {
   railsize;
   Slots;
 
-  constructor(obj) {
-    Precio = obj.Precio ?? 0;
-    Producto = obj.Producto ?? "Ninguno";
-    railsize = obj.railsize ?? 8;
-    Slots = obj.Slots ?? [];
+  constructor(precio1, producto1, railsize1, slots1) {
+    this.Precio = precio1 ?? 0;
+    this.Producto = producto1 ?? "Ninguno";
+    this.railsize = railsize1 ?? 8;
+    this.Slots = slots1 ?? [];
   }
   extraer() {
     if (this.Slots.length < 1) {
       throw new SyntaxError("carril vacio, nada que sacar");
     }
-    return Slots.pop();
+    return this.Slots.pop();
   }
 
   rellenar(lista) {
     if (this.Slots.length + lista.length > this.railsize) {
       throw new SyntaxError("carril sobrecargado");
     }
-    Slots = this.Slots.concat(lista);
-    return Slots;
+    this.Slots = this.Slots.concat(lista);
+    return this.Slots;
   }
 }
 //5.3 la maquina
 class monedero {
   dinero;
-  constructor(obj) {
-    dinero = obj.dinero ?? 0;
+  constructor(dineroinicial) {
+    this.dinero = dineroinicial ?? 0;
   }
   addmoney(money) {
     this.dinero = this.dinero + money;
@@ -77,62 +77,80 @@ class maquina {
   matriz;
   monederomaquina;
 
-  constructor(obj) {
-    tipocarril = obj.tipocarril ?? new carril();
-    filas = obj.filas ?? 10;
-    columnas = obj.columnas ?? 10;
-    matriz = obj.matriz ?? [];
-    monederomaquina = obj.monederomaquina ?? new monedero();
+  constructor(tipocarril1, filas1, columnas1, matriz1, monederomaquina1) {
+    this.tipocarril = tipocarril1 ?? new carril();
+    this.filas = filas1 ?? 10;
+    this.columnas = columnas1 ?? 10;
+    this.matriz = matriz1 ?? [];
+    this.monederomaquina = monederomaquina1 ?? new monedero();
 
-    this.matriz = new Array(filas);
+    this.matriz = new Array(this.filas);
     for (let i = 0; i < this.filas; i++) {
-      this.matriz[i] = new Array(columnas);
+      this.matriz[i] = new Array(this.columnas);
       for (let j = 0; j < this.columnas; j++) {
         this.matriz[i][j] = new carril();
       }
     }
   }
   updateFilaName(posicion, name) {
-    fila = Number(posicion[0]) - 1;
-    columna = Number(posicion[1]) - 1;
+    let fila = Number(posicion[0]) - 1;
+    let columna = Number(posicion[1]) - 1;
     return (this.matriz[fila][columna].Producto = name);
   }
 
   updateFilaPrice(posicion, precio) {
-    fila = Number(posicion[0]) - 1;
-    columna = Number(posicion[1]) - 1;
+    let fila = Number(posicion[0]) - 1;
+    let columna = Number(posicion[1]) - 1;
     return (this.matriz[fila][columna].Precio = precio);
   }
 
   consultar(posicion) {
+    let fila = Number(posicion[0]) - 1;
+    let columna = Number(posicion[1]) - 1;
     if (
-      this.matriz[fila][columna].Producto === "Ninguno" ||
-      this.matriz[fila][columna].Precio === 0
-    )
-      fila = Number(posicion[0]) - 1;
-    columna = Number(posicion[1]) - 1;
-    return (
-      this.matriz[fila][columna].Producto +
-      "a" +
-      (this.matriz[fila][columna].Precio + "")
-    );
+      this.matriz[fila][columna].Producto !== "Ninguno" ||
+      this.matriz[fila][columna].Precio !== 0
+    ) {
+      let fila = Number(posicion[0]) - 1;
+      let columna = Number(posicion[1]) - 1;
+      return console.log(
+        this.matriz[fila][columna].Producto +
+          " a " +
+          (this.matriz[fila][columna].Precio + "")
+      );
+    } else {
+      return console.log("producto no disponible");
+    }
   }
 
   comprar(posicion) {
-    fila = Number(posicion[0]) - 1;
-    columna = Number(posicion[1]) - 1;
+    let fila = Number(posicion[0]) - 1;
+    let columna = Number(posicion[1]) - 1;
 
     if (
       this.monederomaquina.compruebamoney(this.matriz[fila][columna].Precio)
     ) {
-      return "entregado item " + this.matriz[fila][columna].extraer()+"cambio de "+this.monederomaquina.dinero;
+      return console.log(
+        "entregado item " +
+          this.matriz[fila][columna].extraer() +
+          " // cambio de " +
+          this.monederomaquina.dinero
+      );
+    } else {
+      return console.log("compra fallida");
     }
-    else {return "compra fallida";}
   }
 }
 
 //probando cosas
-//let mimaquina=new maquina;
-//mimaquina.updateFilaName("26","emanems")
-let purse = new monedero();
-console.log(purse.dinero)
+let mimaquina = new maquina();
+mimaquina.matriz[1][5].rellenar("m&m's");
+mimaquina.updateFilaName("26", "emanems");
+mimaquina.updateFilaPrice("26", 3);
+mimaquina.consultar("71");
+mimaquina.consultar("26");
+mimaquina.comprar("26");
+//mimaquina.comprar("71");
+mimaquina.monederomaquina.addmoney(6);
+mimaquina.comprar("26");
+mimaquina.comprar("26");
