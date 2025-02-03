307b386259afceb90d57ce67eac87122874a1d8c
Adolfo Reyes Agustin <50199525+adolecumberri@users.noreply.github.com>
2025-02-03 20:53:01 +0100

    Update ejercicio5.js

--- armando/ejercicio5.js
+++ armando/ejercicio5.js
@@ -70,7 +70,7 @@ class Monedero {
     this.dinero = dineroinicial ?? 0;
   }
   addmoney(money) {
-    this.dinero = this.dinero + money;
+    this.dinero = this.dinero + money; //"x = x + y"  se puede escribir como "x += y"
     return this.dinero;
   }
   compruebamoney(money) {
@@ -127,8 +127,8 @@ class Maquina {
     if (
       this.matriz[fila][columna].Producto !== "Ninguno" ||
       this.matriz[fila][columna].Precio !== 0
-    ) {
-      let fila = Number(posicion[0]) - 1;
+    ) { // esta condicion se puede resumir en que la posicion de la matriz no este vacia.
+      let fila = Number(posicion[0]) - 1; //estos valores ya los has creado encima del if.
       let columna = Number(posicion[1]) - 1;
       return console.log(
         this.matriz[fila][columna].Producto +
@@ -194,6 +194,9 @@ const fillRailWithProduct = (rail, product) => {
 *  3.1 el constructor es confuso. podria hacerse que recibiera un objero
 *    ejemplo: new Carril(,,10,[]);
 *  3.2 el rail no acepta un solo item.
+*  3.3 Los errores deberian ser mensajes para que las maquina los pasase.
+*  3.4 product es un objeto tipo producto, no deberia ser objeto o string.
+en vez de un string por defecto, que sea un objeto con valores por defecto.
 */
 
 // creo el carril con el que voy a rellenar la maquina
@@ -207,4 +210,89 @@ rail.rellenar([donetes]);
 console.log("5-", rail.Slots);
 console.log(rail.Slots)
 
-let machine = new Maquina();
+//uso idilico del carril:
+let myRail = new Rail(3); //mi carril se instancia con el tamaño maximo.
+//SET UP
+myRail.productName("fanta to rica");
+myRail.price = 300; // he decidido que voy a trabajar en centimos para no gestionar internamente decimales
+
+
+myRail.addItem({ name: "fanta" });
+console.log(myRail.products.length); // 1. 
+
+myRail.addItems([
+  { name: "fanta" },
+  { name: "fanta" }
+]); // este esta en plural porque me deja añadir mas
+console.log(myRail.products.length); // 3. 
+
+myRail.extract(); // {name: "fanta"}
+console.log(myRail.products.length); // 2
+
+myRail.addItems([
+  { name: "fanta" },
+  { name: "fanta" }
+]); // Max capacity of the rail reached.
+
+
+/**
+ * 4. Monedero
+ * 4.1 compruebamoney no cumple el dogma de que cada funcion debe hacer una sola cosa:
+ * sustrae dinero y comprueba si se puede comprar, si hay un addMoney, deberia haber un substractMoney
+ * 4.2 deberia haber una funcion para desplegar el monedero actual.
+ * 4.3 el monedero se ha tragado mi monera.
+ * 
+ */
+
+// uso idilico del monedero:
+let purse = new Purse();
+purse.add(100); // trabaja en centimos
+
+purse.displayCash(); // 1.00 €
+
+purse.checkIfAllowed(200) // false
+
+//aqui la maquina envia un mensaje.
+purse.checkIfAllowed(99) //true
+
+purse.substract(99);
+
+purse.returnChange(); // devuelve 1 y se vacia.
+
+
+/**
+ * 5. Maquina,
+ * 5.1 matriz no tiene sentido como parametro, porque es un valor que creas.
+ * 5.2 tenemos que hablar de scopes:
+ *  let fila = Number(posicion[0]) - 1; 
+    let columna = Number(posicion[1]) - 1;
+  5.3 aislaria la logica de obtener fila y columna del string en una sola funcion
+  ejemplo: this.split("45"); // {row: 4, column: 5}
+ */
+
+let machine = new Maquina(undefined, 4, 4, undefined, undefined);
+
+//uso idilico de la maqiuna.
+/**
+ * la logica de la maquina me parece bien, en ella tengo poca remarca.
+ */
+
+let m = new Machine({
+  columns: 4,
+  rows: 4,
+  length: 5 // esta length va a ir directa al rail
+});
+// el constructor genera una matriz de 4x4 con length = 5
+
+//SET UP
+Machine.updateRail("11", {
+  name: "mnm´s",
+  price: 120 //internamente trabajo en centimos, ya sabes.
+});
+
+Machine.updateRail("12", {
+  name: "fanta",
+  price: 210 //internamente trabajo en centimos, ya sabes.
+})
+
+
